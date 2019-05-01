851cccbfb804a6d0d0b1a2083b2574fbf44a6b22
Yepez1997 <yepez4845@gmail.com>
2019-04-30 21:17:05 -0700

    add

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,6 +12,7 @@ import { logout } from '../actions/session_actions';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
     <div>
@@ -19,6 +20,7 @@ const App = () => (
             <Link to="/" className="header-link">
                 <h1>Lyrical Genius</h1>
             </Link>
+            <GreetingContainer />
         </header>
         <Switch>
             <AuthRoute exact path="/login" component={LogInFormContainer} />
--- frontend/lyrical.jsx
+++ frontend/lyrical.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 import { logout } from './util/session_api_util';
-import { log } from 'util';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
