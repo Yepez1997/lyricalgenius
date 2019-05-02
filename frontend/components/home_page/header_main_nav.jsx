4136a800368fc47e6253bf02339690ae4ce62316
Yepez1997 <yepez4845@gmail.com>
2019-05-01 21:55:52 -0700

    user auth done needs some fixes tho

--- frontend/components/home_page/header_main_nav.jsx
+++ frontend/components/home_page/header_main_nav.jsx
@@ -15,19 +15,6 @@ const HeaderMainNav = ({currentUser, logout, modal}) => {
     </>
   );
 
-   const sessionLinks = () => (
-    <nav className="login-signup">
-      <button onClick={() => openModal('login')}>Login</button>
-      &nbsp;or&nbsp;
-      <button onClick={() => openModal('signup')}>Signup</button>
-    </nav>
-  );
-  const personalGreeting = () => (
-    <hgroup className="header-group">
-      <h2 className="header-name">Hi, {currentUser.username}!</h2>
-      <button className="header-button" onClick={logout}>Log Out</button>
-    </hgroup>
-  );
 
   const showButton = currentUser ? logOut(currentUser, logout) : signInSignUp();
 
