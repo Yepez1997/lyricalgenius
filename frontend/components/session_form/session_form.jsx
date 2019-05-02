cb715708d3b00b5ce88a0c78f5432e8a0378b136
Yepez1997 <yepez4845@gmail.com>
2019-05-02 12:04:11 -0700

    push to heroku

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -22,6 +22,7 @@ class SessionForm extends React.Component {
     e.preventDefault();
     const user = Object.assign({}, this.state);
     this.props.processForm(user);
+    // if not sucess do not close modal 
     this.props.closeModal();
   }
  // want to clear errors here ? 
