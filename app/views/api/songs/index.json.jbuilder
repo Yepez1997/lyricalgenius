a7a50c33a111e3ccd821cb667c1ec929e296bf10
Yepez1997 <yepez4845@gmail.com>
2019-05-03 15:04:32 -0700

    Adding way more stuff

--- app/views/api/songs/index.json.jbuilder
+++ app/views/api/songs/index.json.jbuilder
@@ -4,6 +4,7 @@
     json.set! song.id do 
         json.title song.title
         json.artist song.artist.name 
+        json.artist_verified song.artist.is_verified
         json.genre song.genre
     end
 end 
--- frontend/components/home_page/top_charts.jsx
+++ frontend/components/home_page/top_charts.jsx
@@ -14,13 +14,17 @@ class TopCharts extends React.Component {
     render() {
         //TODO: INJECT ALL DATA FROM DATABASE HERE 
         const songs = this.props.songs.map((song, index) => {
+          let verified = song.artist_verified ? '✅' : " ";
+          debugger
           return (
             <ul className="songInfo" key={song.id}>
               <li className="songInfo-number"> {index + 1} </li>
               <li className="songInfo-image"> Dummy-Info </li>
               <li className="songInfo-name">
                 <span className="songInfo-title">{song.title}</span>
-                <span className="songInfo-artist">{song.artist}</span>
+                <span className="songInfo-artist">
+                  {song.artist} {verified}
+                </span>
               </li>
             </ul>
           );
