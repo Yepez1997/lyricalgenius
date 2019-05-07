d8130d7ec89d60bbdaaec770ed6f2961f3e1d0e8
Yepez1997 <yepez4845@gmail.com>
2019-05-06 17:36:09 -0700

    able to fake save git add .! now need to create reducers and save the annotations to the actual db

--- frontend/components/annotation_form/annotation_form.jsx
+++ frontend/components/annotation_form/annotation_form.jsx
@@ -5,8 +5,21 @@ class AnnotationForm extends React.Component {
     constructor(props) {
         super (props);
         this.state = this.props.annotation;
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    update(field) {
+        return (e) => {
+        this.setState({[field]: e.target.value})
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        // TODO -> should dispatch and create a state 
+        console.log("FAKE SAVED THIS");
+        this.setState({body:""});
+    }
 
     render() {
         // FIXME: add onchange handlers 
@@ -14,13 +27,14 @@ class AnnotationForm extends React.Component {
 
             <div className="form">
                 <div className="form-body">
-                    <textarea 
+                    <textarea onChange={this.update('body')}
+                    value={this.state.body}
                     rows="10"
                     name="text area"
                     placeholder=" Don't just put the lyric in your own words -- drop some knowledge "> </textarea>
                 </div>
                 <div className="form-button">
-                    <button className="save"> Save (+15 IQ)</button>
+                    <button onClick={this.handleSubmit}className="save"> Save (+15 IQ)</button>
                     <button className="cancel"> Cancel </button>
                 </div>
             </div>
--- frontend/components/annotation_form/links_annotation_state_container.jsx
+++
@@ -1,8 +0,0 @@
-import {connect} from 'react-redux';
-
-
-const mapStateToProps = (state) => ({
-    links: Object.key(state.entities.links).map(id => state.entities.links[id])
-});
-
-export default connect(mapStateToProps)()
