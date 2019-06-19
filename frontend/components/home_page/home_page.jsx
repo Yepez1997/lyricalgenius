a0e76bd9f7fe2590939d1c7cb030d30750a2e8d0
Yepez1997 <yepez4845@gmail.com>
2019-06-19 16:34:54 -0700

    almost done

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -17,12 +17,12 @@ const HomePage = () => (
             <span> News </span>
             <span className="border-span"> </span>
           </div>
-          <h2>
+          <a href="https://genius.com/a/roc-nation-ceo-calls-for-an-end-to-album-bundles-says-billboard-is-trying-to-blunt-streaming-s-momentum">
             Roc Nation CEO Calls For An End To Album Bundles & Says ‘Billboard’
             Is Trying To “Blunt Streaming’s Momentum
-          </h2>
+          </a>
           <h6>She’s still unhappy about DJ Khaled debuting at No. 2.</h6>
-          <h1> TEXT </h1>
+          <h5 className="news-author"> by Chris Mench / June 19 2019 </h5>
         </div>
         <img src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2018%2F03%2Fdj-khaled-father-of-asahd-video-letter-0.jpg?w=960&cbr=1&q=90&fit=max" />
       </div>
