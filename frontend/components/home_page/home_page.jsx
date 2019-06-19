f42d679587a5f3016bf201f7e853ccfe1a932d2e
Yepez1997 <yepez4845@gmail.com>
2019-06-19 16:21:27 -0700

    ....

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -15,11 +15,13 @@ const HomePage = () => (
         <div className="header-news-title">
           <div className="news-bar">
             <span> News </span>
+            <span className="border-span"> </span>
           </div>
           <h2>
             Roc Nation CEO Calls For An End To Album Bundles & Says ‘Billboard’
             Is Trying To “Blunt Streaming’s Momentum
           </h2>
+          <h6>She’s still unhappy about DJ Khaled debuting at No. 2.</h6>
           <h1> TEXT </h1>
         </div>
         <img src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2018%2F03%2Fdj-khaled-father-of-asahd-video-letter-0.jpg?w=960&cbr=1&q=90&fit=max" />
